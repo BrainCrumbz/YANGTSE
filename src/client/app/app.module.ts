e8b6faa74e87b6053746c741800da2e55614e9f2
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2016-10-17 16:08:04 +0200

    chore(build): re-enable `require()` of global CSS

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -9,12 +9,11 @@ import { AppComponent } from '../app/app.component';
 import { appRouting } from '../app/app.routing';
 import { appProviders } from '../app/app.providers';
 
-/* TODO None of these works with AOT production build. At least for `require`,
- * there seems to be issues with AOT.
 // Here import global styles common to all app modules
-require('../shared/common-styles.css');
-*/
+/*
 import '../shared/common-styles.css';
+*/
+require('../shared/common-styles.css');
 
 @NgModule({
   imports: [
